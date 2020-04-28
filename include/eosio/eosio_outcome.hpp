bdb9de84d1d7f7605d15cf3766771bbb3db14c74
larryk85 <larry.kittinger@block.one>
2020-04-28 11:11:48 -0400

    update outcome and disable it for now

--- include/eosio/eosio_outcome.hpp
+++ include/eosio/eosio_outcome.hpp
@@ -38,4 +38,43 @@ void check_discard(result<T> r) {
       check(r.error());
 }
 
+namespace detail {
+   template <typename T>
+   constexpr std::true_type is_outcome_result(result<T>) { return {}; }
+   template <typename T>
+   constexpr std::false_type is_outcome_result(T) { return {}; }
+
+   template <typename T>
+   constexpr inline static bool is_outcome_result_v =
+      std::is_same_v<decltype(is_outcome_result(std::declval<T>())), std::true_type>;
+
+   template <typename T>
+   decltype(auto) outcome_value_forwarder(T&& val) {
+      if constexpr (is_outcome_result_v<T>)
+         return val.value();
+      //else
+      //   return std::forward<T>(val);
+   }
+} // namespace eosio::detail
 } // namespace eosio
+
+//#ifndef ABIEOS_ASSERT_ERRORS
+//#define EOSIO_TRY OUTCOME_TRY
+//#else
+//#define EOSIO_OUTCOME2(VAR, EXP) \
+//   auto VAR = eosio::detail::outcome_value_forwarder(EXP);
+//#define EOSIO_OUTCOME1(EXP)
+//
+//#define GET_MACRO(_1, _2, NAME, ...) NAME
+//#define EOSIO_TRY(...) GET_MACRO(__VA_ARGS__, \
+//      EOSIO_OUTCOME2, EOSIO_OUTCOME1)(__VA_ARGS__)
+//
+//#endif
+
+#define EOSIO_OUTCOME2(VAR, EXP) \
+   auto VAR = eosio::detail::outcome_value_forwarder(EXP);
+#define EOSIO_OUTCOME1(EXP)
+
+#define GET_MACRO(_1, _2, NAME, ...) NAME
+#define OUTCOME_TRY(...) GET_MACRO(__VA_ARGS__, \
+      EOSIO_OUTCOME2, EOSIO_OUTCOME1)(__VA_ARGS__)
